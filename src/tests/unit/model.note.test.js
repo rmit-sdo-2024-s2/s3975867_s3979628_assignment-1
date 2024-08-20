083603968683cf855ef1f1167f3f04f44c1002a8
s3975867 <130524469+s3975867@users.noreply.github.com>
2024-08-20 10:57:15 +1000

    Update model.note.test.js

--- src/tests/unit/model.note.test.js
+++ src/tests/unit/model.note.test.js
@@ -1,23 +1,22 @@
 const mockingoose = require('mockingoose');
-const Note = require("../../models/note")
+const Note = require("../../models/note");
 
 // Unit Tests only test the validation built into Note
-
 describe('mockingoose', () => {
     beforeEach(() => {
         mockingoose.resetAll();
-        jest.clearAllMocks()
-    })
+        jest.clearAllMocks();
+    });
 
     describe("Test Both Fields Are Set", () => {
         it('Validate Model', async () => {
             const todo = new Note({
                 title: "Task Note",
-                description: "" // "This can't be blank"
+                description: "This is a valid description" // Ensure description is not empty
             });
 
             const result = await todo.validateSync();
-            expect(result).toBe(undefined);
+            expect(result).toBe(undefined); // Expect no validation errors
         });
-    })
-})
+    });
+});
